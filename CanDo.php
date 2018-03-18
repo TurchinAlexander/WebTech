f7636eb80e9a8e5d93905630edf7265ef26ad6eb
Александр Турчин <Nestor312@mail.ru>
2018-03-18 04:32:17 +0300

    Initial commit

---
+++ CanDo.php
@@ -0,0 +1,89 @@
+<html>
+  <head>
+    <title>Can you do this?</title>
+    <link href="./styles.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Главная, История, Мемы">
+    <meta name="description" content="Описание разделов">
+  </head>
+  <body>
+  <!-- Заголовок для навигации -->
+    <div class="nav">
+        <ul class="sections" style="border-bottom-color: black">
+          <li><a href="./Main.php">Главная</a></li>
+          <li><a href="./Photo.php">Фото</a></li>
+          <li><a href="./Video.php">Видео</a></li>
+          <li><a href="./Archive.php">Архив</a></li>
+          <li><a href="./Contacts.php">Контакты</a></li>
+        </ul>
+    </div>
+
+    <!-- Блок с описаниями -->
+    <table style="padding-top: 10px">
+      <tr>
+        <td class="empty">
+        </td>
+        <td class="info">
+          <h1>Мем Can you do this?</h1>
+          <div>
+            <h2>История его появления</h2>
+            <p>
+              Данный мем был представлен ютубером Pewdiepie как обычная шутка про геймерское кресло,
+              которое у него появилось. Основное отличие этого кресла от большинства остальных в том, что
+              оно может раскладываться на 180 градусов. И при каждой ситуации, где говориться про особенность
+              какой-либо вещи, Pewdiepie говорит: "Can you do this?" и раскладывает свое кресло с намеком, что
+              у тебя такого кресла нет.
+            </p>
+          </div>
+          <p style="text-align:justify">&nbsp;</p>
+
+          <div>
+            <h2>Примеры использования</h2>
+            <iframe width="1120" height="630" src="https://www.youtube.com/embed/v8eIuhPstno" frameborder="0"
+              allow="autoplay; encrypted-media" allowfullscreen>
+            </iframe>
+            <p style="text-align:justify">&nbsp;</p>
+            <iframe width="1120" height="630" src="https://www.youtube.com/embed/gpWSyFz9Cd0" frameborder="0"
+              allow="autoplay; encrypted-media" allowfullscreen>
+            </iframe>
+            <p style="text-align:justify">&nbsp;</p>
+          </div>
+
+        </td>
+		
+        <td>
+          <!-- Первая новость -->
+          <a href="./Uganda.php">
+            <div class="blockpic">
+              <img src="./pic/UgandaKnukle.png" class="pic">
+              <p>
+                Был добавлен мем <br>Uganda Warrior!
+              </p>
+            </div>
+          </a>
+
+          <!-- Вторая новость -->
+          <a href="./Spaghet.php">
+            <div class="blockpic">
+              <img src="./pic/SmbdSpaget.png" class="pic">
+              <p>
+                Был добавлен мем <br>Somebody Touch My Spaget!
+              </p>
+            </div>
+          </a>
+
+          <!-- Третья новость -->
+          <a href="./CanDo.php">
+            <div class="blockpic">
+              <img src="./pic/CanDoThis.jpg" class="pic">
+              <p>
+                Был добавлен мем <br>Can You Do This? !
+              </p>
+            </div>
+          </a>
+
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
